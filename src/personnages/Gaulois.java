2e3909c3ceff19b1d943e2804e3ee14a33e10a50
GRL4714A <GRL4714A@u3-213-06.ens.fsi>
2023-02-14 19:42:21 +0100

    Création classe gaulois.

---
+++ src/personnages/Gaulois.java
@@ -0,0 +1,47 @@
+package personnages;
+
+public class Gaulois {
+	private String nom;
+	private int force;
+	private int effetPotion = 1;
+	
+	public Gaulois(String nom, int force) {
+		this.nom = nom;
+		this.force = force;
+	}
+	
+	public void parler(String texte) {
+		System.out.println(prendreParole() + "« " + texte + "»");
+	}
+
+	private String prendreParole() {
+		return "Le gaulois " + nom + " : ";
+	}
+	
+	
+	
+	public String getNom() {
+		return nom;
+	}
+
+	public int getForce() {
+		return force;
+	}
+
+	public void setForce(int force) {
+		this.force = force;
+	}
+
+	public int getEffetPotion() {
+		return effetPotion;
+	}
+
+	@Override
+	public String toString() {
+		return "Gaulois [nom=" + nom + ", force=" + force + ", effetPotion=" + effetPotion + "]";
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+}
