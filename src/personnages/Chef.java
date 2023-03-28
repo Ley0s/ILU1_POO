df75febdb445ef95e1f121969f8712665df67899
GRL4714A <GRL4714A@u3-213-05.ens.fsi>
2023-03-28 16:49:52 +0200

    Complétion des classes Village et Chef.

--- src/personnages/Chef.java
+++ src/personnages/Chef.java
@@ -3,19 +3,33 @@ package personnages;
 public class Chef {
 	private String nom;
 	private int force;
-	private int effetPotion = 1;
+	private int effetPotion = 0;
 	private Village village;
 	
-	public Chef(String nom, int force, int effetPotion, Village village) {
-		super();
+	public Chef(String nom, int force, Village village) {
 		this.nom = nom;
 		this.force = force;
-		this.effetPotion = effetPotion;
 		this.village = village;
 	}
 
 	public String getNom() {
 		return nom;
+	}
+	
+	public int getForce() {
+		return force;
+	}
+
+	public int getEffetPotion() {
+		return effetPotion;
+	}
+
+	public void setForce(int force) {
+		this.force = force;
+	}
+	
+	public void setVillage(Village village) {
+		this.village = village;
 	}
 	
 	public void parler(String texte) {
@@ -29,6 +43,12 @@ public class Chef {
 	public void frapper(Romain romain) {
 		System.out.println(nom + " envoie un grand coup dans la machoire de "
 				+ romain.getNom());
-		romain.recevoirCoup(force / 3);
+		romain.recevoirCoup((force / 3) * effetPotion);
 	}
+	
+	public void boirePotion(int forcePotion) {
+		effetPotion = forcePotion;
+		parler("Merci Druide, je sens que ma force est " + forcePotion + " fois décuplée");
+	}
+
 }
--- src/personnages/Village.java
+++ src/personnages/Village.java
@@ -13,6 +13,7 @@ public class Village {
 
 	public void setChef(Chef chef) {
 		this.chef = chef;
+		chef.setVillage(this);
 	}
 
 	public String getNom() {
@@ -28,15 +29,28 @@ public class Village {
 		return villageois[indice];
 	}
 	
+	public void afficherVillageois() {
+		System.out.println("Dans le village du chef " + chef.getNom() + " vivent les gaulois :");
+		for(int i=0; i<nbVillageois; i++) {
+			System.out.println("- " + villageois[i].getNom());
+		}
+	}
+	
 	public static void main(String[] args) {
-		Village villageDesIrreductibles = new Village("Village des Irréductibles", 30);
-		Chef abraracourcix;
-		villageDesIrreductibles.setChef(abraracourcix);
-		abraracourcix = new Chef("Abraracourcix", 6, villageDesIrreductibles);
+		Village village = new Village("Village des Irréductibles", 30);
+		Chef abraracourcix = new Chef("Abraracourcix", 6, village);
+		village.setChef(abraracourcix);
+		Gaulois asterix = new Gaulois("Astérix", 8);
+		village.ajouterHabitant(asterix);
+		Gaulois obelix = new Gaulois("Obélix", 25);
+		village.ajouterHabitant(obelix);
+		village.afficherVillageois();
 		
-		
+//		Gaulois gaulois = village.trouverHabitant(1);
+//		System.out.println(gaulois);
+//		Ces lignes affichent null car l'habitant d'indice 1 est le deuxième habitant, or il n'y a que Astérix dans le village.
 //		Gaulois gaulois = villageDesIrréductibles.trouverHabitant(30);
-//		Cette ligne donne une erreur car l'indice max est 29 et non 30
+//		Cette ligne donne une erreur car l'indice max est 29 et non 30.
 		
 	}
 }
